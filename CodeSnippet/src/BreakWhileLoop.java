ec5af918c43fcc914beb614fc3eec9b16c6a2881
NIKHILESH TN <NIKHILESH TN@DESKTOP-RILSJLK>
2022-08-27 19:45:26 -0800

    first commit

---
+++ CodeSnippet/src/BreakWhileLoop.java
@@ -0,0 +1,15 @@
+
+public class BreakWhileLoop {
+
+	public static void main(String[] args) {
+		int a=1;
+		while(a<10) {
+			if(a==5)
+				break;
+			System.out.println("\n\statement:"+a);
+			a++;
+		}
+System.out.println("\n\t end of the program");
+	}
+
+}
